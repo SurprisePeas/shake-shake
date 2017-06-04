572c53ab3085053dbe2651dd06b72c63621c831d
SurprisePeas <surprisepeas@qq.com>
2017-06-04 16:45:46 +0800

    Shake🦁Shake Setting Sail~

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shake 🦁 Shake</title>
+
+    <meta name="description" content="{{ config('app.tagline') }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="mobile-web-app-capable" content="yes">
+
+    <meta name="theme-color" content="#17abe3">
+    <meta name="msapplication-navbutton-color" content="#17abe3">
+
+</head>
+<body>
+<div id="app">
+    hello
+</div>
+</body>
+</html>
